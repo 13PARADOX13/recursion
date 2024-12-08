76c9f8145b62e331a6cec5e046b80cd47f2f3a76
13PARADOX13 <wdg42ru@gmail.com>
2024-12-08 16:29:48 +0700

    recursion.py

--- recursion.py
+++ recursion.py
@@ -5,6 +5,5 @@ def get_multiplied_digits(number ):
         return first
     else:
         return first * get_multiplied_digits(int(str_number[1:]))
-    # print(first)
 
 print(get_multiplied_digits(12345))
